5cefdc38b9910fa92218e52f6d98311f571230a1
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-08-14 00:03:18 +0530

    add this backend ride route

--- backend/controller/rideControllers.js
+++ backend/controller/rideControllers.js
@@ -32,3 +32,39 @@ module.exports.createRide=async(req,res,next)=>{
     }
 
 
+    module.exports.getFare=async(req,res,next)=>{
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                status:'fail',
+                errors:errors.array()
+            });
+        }
+        const {pickup,destination}=req.query;
+
+        try {
+            const fare = await rideService.getFare({pickup,destination});
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    fare
+                }
+            });
+        } catch (error) {
+            res.status(500).json({error:error.message})
+        }
+    }
+
+    module.exports.confirmRide=async(req,res,next)=>{
+
+    }
+
+    module.exports.startRide=async(req,res,next)=>{
+
+    }
+
+    module.exports.endRide=async(req,res,next)=>{
+
+    }
+
+
--- backend/routes/rideRouter.js
+++ backend/routes/rideRouter.js
@@ -2,7 +2,7 @@ const express=require("express");
 const rideController=require("../controller/rideControllers")
 
 const router=express.Router()
-const {body}=require("express-validator")
+const {body,query}=require("express-validator")
 const authmiddleware=require("../middlewares/authMiddlewares")
 
 router.post('/create',authmiddleware.authUser,
@@ -13,6 +13,33 @@ router.post('/create',authmiddleware.authUser,
     (req,res,next)=>{
         rideController.createRide(req,res,next);
     }
+)
+router.get('/get-fare',
+    query('pickup').isString().isLength({ min: 3 }).withMessage('Invalid pickup address'),
+    query('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
+    (req,res,next)=>{
+        rideController.getFare(req,res,next);
+    }
+)
+    
+
+router.post('/confirm',
+    authmiddleware.authCaptain,
+    body('rideId').isMongoId().withMessage('Invalid ride id'),
+    rideController.confirmRide
+)
+
+router.get('/start-ride',
+    authmiddleware.authCaptain,
+    query('rideId').isMongoId().withMessage('Invalid ride id'),
+    query('otp').isString().isLength({ min: 6, max: 6 }).withMessage('Invalid OTP'),
+    rideController.startRide
+)
+
+router.post('/end-ride',
+    authmiddleware.authCaptain,
+    body('rideId').isMongoId().withMessage('Invalid ride id'),
+    rideController.endRide
 )
 
 module.exports=router;
