3725bb74cde221fba96e6248b02d927f13264b6b
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-08-15 22:46:30 +0530

    captain

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API_URL = "http://localhost:8080";
 
 
+
+
--- frontend/src/Captain/CaptainGetProfile.jsx
+++ frontend/src/Captain/CaptainGetProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CaptainGetProfile = () => {
   return (
     <div>
-      
+      <h1>Captain Profile</h1>
     </div>
   )
 }
